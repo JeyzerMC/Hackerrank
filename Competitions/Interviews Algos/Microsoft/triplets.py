70ea59f018ffd2d5b48974cd69c461f5b1741c5b
mechae <mehdi.chaid@polymtl.ca>
2017-11-21 17:08:43 -0500

    oui

--- Competitions/Interviews Algos/Microsoft/triplets.py
+++ Competitions/Interviews Algos/Microsoft/triplets.py
@@ -1,13 +1,13 @@
 def nbTriplets(n, arr):
     arr, nbOcc = sorted(arr), 0
     for i in range(len(arr)):
-        if i > n:
+        if arr[i] > n:
             break
         for j in range(i+1, len(arr)):
-            if i + j > n:
+            if arr[i] + arr[j] > n:
                 break
             for k in range(j+1, len(arr)):
-                if i + j + k > n:
+                if arr[i] + arr[j] + arr[k] > n:
                     break
                 nbOcc += 1
     return nbOcc
@@ -15,5 +15,4 @@ def nbTriplets(n, arr):
 
 n = int(input())
 arr = list(map(int, input().split(' ')))
-
 print(nbTriplets(n, arr))
